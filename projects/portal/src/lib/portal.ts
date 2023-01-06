fb336fa3b703793eb7b6e48d84ce456095d6f924
Doai <nguyendoai7777@gmail.com>
2023-01-06 14:56:47 +0700

    add animation option and change docs

--- projects/portal/src/lib/portal.ts
+++ projects/portal/src/lib/portal.ts
@@ -1,21 +1,50 @@
-import { Directive, ElementRef, inject, Input, NgModule, OnChanges, OnDestroy, Renderer2, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, inject, Input, OnChanges, OnDestroy, Renderer2, SimpleChanges } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 /**
- * This directive append to body as default.
- *
- * Use case:  modal, popup, popover, dialog, etc.
+ * This directive append to body and use for standalone components as default.
+ * Use case:  modal, popup, dialog, etc.
  *
  * How to use:
  *
  * <div [portal]="yourStatement"></div>
  *
- * @use use for standalone components by default, if use with module, remove `standalone: true` in @decorator declaration
+ * If you want to use animation, class [`component-mount`] will be attached when component show, opposite class [`component-destroy`] will be attached when component destroy.
+ * You can use `destroyDelay` to control animation duration, ex:
+ *
+ * <div [portal]="yourStatement" [destroyDelay]="1000">...</div>
+ *
+ * css ex:
+ * ```
+ * @keyframes mount {
+ *   from {
+ *     opacity: 0;
+ *   }
+ *   to {
+ *     opacity: 1;
+ *   }
+ * }
+ * @keyframes destroy {
+ *   from {
+ *     opacity: 1;
+ *   }
+ *   to {
+ *     opacity: 0;
+ *   }
+ * }
+ *
+ * .component-destroy {
+ *   animation: destroy 1s linear forwards;
+ * }
+ *
+ * .component-mount {
+ *   animation: mount 1s linear forwards;
+ * }
+ *```
  *
  * @prop portal boolean
- *
- *
- *  */
+ * @prop  destroyDelay number [ms];
+ */
 
 @Directive({
   selector: '[portal]',
@@ -23,7 +52,9 @@ import { DOCUMENT } from '@angular/common';
 })
 export class PortalDirective implements OnChanges, OnDestroy {
   @Input() portal = true;
+  @Input() destroyDelay = 0;
   dcm = inject(DOCUMENT);
+  timer?: NodeJS.Timeout;
 
   constructor(
     private readonly elr: ElementRef<HTMLDivElement>,
@@ -36,11 +67,22 @@ export class PortalDirective implements OnChanges, OnDestroy {
   ngOnChanges(changes: SimpleChanges) {
     const currentState = changes?.['portal'].currentValue;
     if (currentState) {
+      this.elr.nativeElement.classList.add('component-mount');
+      this.elr.nativeElement.classList.contains('destroy') && this.elr.nativeElement.classList.remove('component-destroy');
       this.rd2.appendChild(this.dcm.body, this.elr.nativeElement);
     } else {
+      this.timer = undefined;
       const existed = this.dcm.body.querySelector('[cpn-type="portal"]');
       if (existed) {
-        this.rd2.removeChild(this.dcm.body, this.elr.nativeElement || existed);
+        if (this.destroyDelay > 0) {
+          this.elr.nativeElement.classList.remove('component-mount');
+          this.elr.nativeElement.classList.add('component-destroy');
+          this.timer = setTimeout(() => {
+            this.rd2.removeChild(this.dcm.body, this.elr.nativeElement || existed);
+          }, this.destroyDelay);
+        } else {
+          this.rd2.removeChild(this.dcm.body, this.elr.nativeElement || existed);
+        }
       }
     }
   }
